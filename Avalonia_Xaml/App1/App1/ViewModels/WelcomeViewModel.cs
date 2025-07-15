32c866a4136d2a0bf7250bb30bc53caea6d5eec6
Abyssalite <minhhuy.nguyen0909@gmail.com>
2025-07-15 23:06:22 +0200

    Fix

---
+++ Avalonia_Xaml/App1/App1/ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+
+namespace App1.ViewModels;
+
+public partial class WelcomeViewModel : ViewModelBase
+{
+    public string Text { get; }
+    public WelcomeViewModel(
+        Store store,
+        INavigatorService navigator,
+        IDialogService dialogService,
+        IChangeStateService stateService,
+        INotificationService notificate) :
+        base(store, navigator, dialogService, stateService, notificate)
+    {
+        Text = store.WelcomeText;
+    }
+    
+    protected override async Task BackOrToggleDrawerAsync()
+    {
+        _stateService.OpenPane(!_stateService.IsPaneOpen);
+        await Task.CompletedTask;
+    }
+}
